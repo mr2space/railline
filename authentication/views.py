d1ba0e71be4fe7c4fe3eb3f4f2ad580abcc0d950
mr2space <princegoswami.space@gmail.com>
2021-12-12 08:27:42 +0530

    edited registration to account and url work

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
 # Create your views here.
-def index():
-    return render('register.html')
+def account():
+    return HttpRequest("Hello world")
