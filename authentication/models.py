8c956e14b01a2a8e855ea9bfbbd088c4127e5d94
mr2space <princegoswami.space@gmail.com>
2022-01-02 20:55:47 +0530

    models changed removed otp field

--- authentication/models.py
+++ authentication/models.py
@@ -14,37 +14,51 @@ from django.db.models.expressions import F
 class userCreations(models.Model):
     
     # variable use to store default values in database 
-    gender_choices =(
-        ('M','Male'),
-        ('F','Female')
-    )
-    
+
     user_name = models.CharField(max_length=10,default='',unique=True,null=False)
-    profile = models.ImageField(
-        upload_to='images/', default='—Pngtree—blue default avatar_5938444.png')
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    passwd = models.CharField(max_length=300,null=False)
+    passwd = models.CharField(max_length=1800,null=False)
     email=models.EmailField(null=False,max_length=254,unique=True)
+    gender_choices = (
+        ('M', 'Male'),
+        ('F', 'Female')
+    )
+    profile = models.ImageField(
+        upload_to='images/', default='—Pngtree—blue default avatar_5938444.png')
     is_above_18 = models.BooleanField(default=False)
-    gender = models.CharField(max_length=1,choices=gender_choices,default='Male')
+    gender = models.CharField(max_length=1, default='Male')
+    phone_no = models.IntegerField( null=False)
+    address = models.CharField(max_length=100, default='', null=True)
+    gender = models.CharField(
+        max_length=1, choices=gender_choices, default='Male')
     address = models.CharField(max_length=100)
-    otp = models.CharField(max_length=5,default='00000',null=False)
+    otp = models.CharField(max_length=5, default='00000', null=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 
 
-class userAddon(models.Model):
+
+
+class userAddon_saving(models.Model):
+    gender_choices = (
+        ('M', 'Male'),
+        ('F', 'Female')
+    )
+
     user_name = models.OneToOneField(User,on_delete=models.CASCADE)
     profile = models.ImageField(
         upload_to='images/', default='—Pngtree—blue default avatar_5938444.png')
-    first_name = models.CharField(max_length=50,null=False)
-    last_name = models.CharField(max_length=50,null=False)
     is_above_18 = models.BooleanField(default=False)
     gender = models.CharField(max_length=1, default='Male')
-    phone_no = models.IntegerField(max_length=12,null=False)
+    phone_no = models.IntegerField(null=False)
     address = models.CharField(max_length=100,default='',null=True)
+    is_above_18 = models.BooleanField(default=False)
+    gender = models.CharField(
+        max_length=1, choices=gender_choices, default='Male')
+    address = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.phone_no
