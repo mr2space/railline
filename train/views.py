fd84b2ebada78eb8f4d3948918c142766587ebd1
mr2space <princegoswami.space@gmail.com>
2022-01-17 10:33:08 +0530

    space

--- train/views.py
+++ train/views.py
@@ -1,3 +1,14 @@
+from django.https import HttpRequest
 from django.shortcuts import render
+from train.trainf.pnr import pnrSearch
 
-# Create your views here.
+
+def pnr(request):
+    pnr = request.POST.get("pnr-box")
+    print(pnr)
+    # result = pnrSearch(pnr)
+    return HttpRequest(pnr)
+def index(request):
+    return render(request,'index.html')
+
+
