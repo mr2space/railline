b10afeb12dc4b6b4b2cd14c8cd5b9982113b0b48
mr2space <princegoswami.space@gmail.com>
2022-02-14 21:02:48 +0530

    finmal touch

--- authentication/registration/addF.py
+++ authentication/registration/addF.py
@@ -6,16 +6,16 @@ import math
 import hashlib
 
 
-
 class registerFunctions():
     userName = ''
     code = ''
     email = ''
+
     def hashPassword(self, raw_password):
         passwd = str(raw_password)
         passwd = hashlib.sha256(passwd.encode())
         return passwd
-    
+
     def otpGenerator(self):
         # variable to store digit use in Otp
         digits = '0123456789'
@@ -24,19 +24,18 @@ class registerFunctions():
             otp += str(math.floor(random.random()*10))
         self.code = otp
         return 1
-    
+
     def sendEmail(self):
         htmly = get_template('user/Email.html')
-        d = { 'userName': self.userName, 'code':self.code}
+        d = {'userName': self.userName, 'code': self.code}
         subject, from_email, to = f'welcome {self.userName}', 'mrspace.pro@gmail.com', self.email
         html_content = htmly.render(d)
         msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
-        msg.attach_alternative(html_content,"text/html")
+        msg.attach_alternative(html_content, "text/html")
         msg.send()
         return 1
-    
+
     def resendOtp(self):
         self.code = self.otpGenerator()
         self.sendEmail()
         return 1
-
--- train/models.py
+++ train/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class trainRecord(models.Model):
+    trainId =models.AutoField(_(""))
+    trainName = models.models.CharField(max_length=50)
+    
