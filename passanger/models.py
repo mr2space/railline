1a5968bd11549db7f4b010837662b03eb0a3a6c8
mr2space <princegoswami.space@gmail.com>
2022-05-12 13:58:25 +0530

    databse model design

--- passanger/models.py
+++ passanger/models.py
@@ -1,3 +1,17 @@
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+class PassangerData(models.Model):
+    username = models.CharField(max_length=10,unique=True)
+    age = models.IntegerField()
+    full_name = models.CharField(max_length=200)
+    seat_count = models.IntegerField()
+    seat_count_by_SL = models.IntegerField()
+    seat_count_by_1A = models.IntegerField()
+    seat_count_by_2A = models.IntegerField()
+    seat_count_by_3A = models.IntegerField()
+    seat_price_by_SL = models.IntegerField()
+    seat_price_by_1A = models.IntegerField()
+    seat_price_by_2A = models.IntegerField()
+    seat_price_by_3A = models.IntegerField()
+    
