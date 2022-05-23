535695e09a12c5c1417a55f7df661dc1b17145b7
mr2space <princegoswami.space@gmail.com>
2022-05-23 17:52:13 +0530

    no changes

--- booking/views.py
+++ booking/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from train import models as trainModel
 from django.contrib.auth.models import User
-
+from django.views import View
 
 @login_required(login_url='/user/login')
 def userBooking(request):
@@ -26,7 +26,32 @@ def userBooking(request):
         'seat_limit': seat_limit['Seat_SL'],
     }
     return render(request,'./booking/form.html',param)
-    
-def bookingPreProcessing(request):
-    pass
+# class CreateCheckoutSessionView(View):
+#     def post(self,request,*args,**kwargs):
+#     product_id = self.kwargs['pk']
+#     product = Product.objects.get(id=product_id)
+#     print(product)
+#     checkout_session = stripe.checkout.Session.create(
+#         line_items=[
+#             {
+#                 'price_data': {
+#                     'currency': 'inr',
+#                     'unit_amount': product.price,
+#                     'product_data': {
+#                         'name': product.name,
+#                     },
+#                 },
+#                 'quantity': 1,
+#             },
+#         ],
+#         metadata={
+#             "product_id": product.id
+#         },
+#         mode='payment',
+#         success_url='http://127.0.0.1:8000/success/',
+#         cancel_url='http://127.0.0.1:8000/cancel/',
+#     )
+#     print(checkout_session.url)
+#     return redirect(checkout_session.url, code=303)
+
 
